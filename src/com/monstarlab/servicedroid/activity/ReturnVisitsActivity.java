e11aee6f34fb424c2ff67ea745761d8a3ccbfbb2
Sean <sean@mcarthurgfx.com>
2010-05-08 17:21:23 -0700

    can edit and delete return visits

--- src/com/monstarlab/servicedroid/activity/ReturnVisitsActivity.java
+++ src/com/monstarlab/servicedroid/activity/ReturnVisitsActivity.java
@@ -1,12 +1,19 @@
 package com.monstarlab.servicedroid.activity;
 
 import android.app.ListActivity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import com.monstarlab.servicedroid.R;
 import com.monstarlab.servicedroid.model.Models.ReturnVisits;
@@ -14,11 +21,10 @@ import com.monstarlab.servicedroid.model.Models.ReturnVisits;
 public class ReturnVisitsActivity extends ListActivity {
 	
 	private static final int MENU_ADD = Menu.FIRST;
-	//private static final int MENU_ADD = Menu.FIRST + 1;
+	private static final int DELETE_ID = Menu.FIRST + 1;
 	
 	private static final String[] PROJECTION = new String[] { ReturnVisits._ID, ReturnVisits.NAME, ReturnVisits.ADDRESS };
-    
-    //private Cursor mCursor;
+	
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,7 +71,37 @@ public class ReturnVisitsActivity extends ListActivity {
 		
 		return super.onOptionsItemSelected(item);
 	}
-
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.delete_call);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case DELETE_ID:
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+			Uri callUri = ContentUris.withAppendedId(getIntent().getData(), info.id);
+			getContentResolver().delete(callUri, null, null);
+			//fillData();
+			return true;
+		}
+		
+		
+		return super.onContextItemSelected(item);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		//super.onListItemClick(l, v, position, id);
+		Uri uri = ContentUris.withAppendedId(getIntent().getData(), id);
+		Intent i = new Intent(Intent.ACTION_EDIT, uri, this, RVEditActivity.class);
+        startActivity(i);
+	}
+	
+	
 	protected void createCall() {
 		Intent i = new Intent(Intent.ACTION_INSERT, getIntent().getData(), this, RVEditActivity.class);
 		startActivity(i);

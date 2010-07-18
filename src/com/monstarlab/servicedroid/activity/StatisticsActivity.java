d9e5c426d7f51dad701e47b82e90d5dbdc845c8f
Sean <sean@mcarthurgfx.com>
2010-07-17 23:41:08 -0700

    fixing infinite loop

--- src/com/monstarlab/servicedroid/activity/StatisticsActivity.java
+++ src/com/monstarlab/servicedroid/activity/StatisticsActivity.java
@@ -16,13 +16,16 @@ import com.monstarlab.servicedroid.R;
 
 public class StatisticsActivity extends Activity {
 	
-	private static final int MENU_EMAIL = Menu.FIRST;
+	private static final int MENU_MONTH = Menu.FIRST;
+	private static final int MENU_YEAR = Menu.FIRST + 1;
+	private static final int MENU_EMAIL = Menu.FIRST + 2;
 	
 	//private TimeUtil mTimeHelper;
 	
 	private static String[] TimeProjection = new String[] { TimeEntries._ID, TimeEntries.DATE, TimeEntries.LENGTH };
 	private static String[] RVProjection = new String[] { ReturnVisits._ID, ReturnVisits.DATE, ReturnVisits.CALL_ID };
 	
+	private TextView mTimePeriodDisplay;
 	private TextView mHoursDisplay;
 	private TextView mRvsDisplay;
 	private TextView mMagsDisplay;
@@ -30,7 +33,8 @@ public class StatisticsActivity extends Activity {
 	private TextView mBooksDisplay;
 	private TextView mBibleStudiesDisplay;
 	
-	private String mCurrentTimePeriod = "06/2010"; //TODO - should be changeable
+	private String mCurrentTimePeriod = "07/2010"; //TODO - should be changeable
+	private int mTimeSpan = MENU_MONTH;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,7 @@ public class StatisticsActivity extends Activity {
         setContentView(R.layout.stats);
         
        // mTimeHelper = new TimeUtil(this);
-        
+        //mTimePeriodDisplay = (TextView)findViewById(R.id.stats_timeperiod);
         mHoursDisplay = (TextView)findViewById(R.id.hours);
         mRvsDisplay = (TextView)findViewById(R.id.rvs);
         mMagsDisplay = (TextView)findViewById(R.id.magazines);
@@ -59,6 +63,7 @@ public class StatisticsActivity extends Activity {
 
 
 	protected void fillData() {
+		//mTimePeriodDisplay.setText(mCurrentTimePeriod);
 		mHoursDisplay.setText(getHoursSum());
 		mRvsDisplay.setText(getRVs());
 		mMagsDisplay.setText(getMagazines());
@@ -102,6 +107,7 @@ public class StatisticsActivity extends Activity {
 			c.moveToFirst();
 			while(!c.isAfterLast()) {
 				sum += c.getInt(2);
+				c.moveToNext(); 
 			}
 			c.close();
 			c = null;
@@ -122,21 +128,38 @@ public class StatisticsActivity extends Activity {
 		return numOfRVs;
 	}
 	
+	protected void setTimePeriod() {
+		
+	}
+	
 	protected String inTimePeriod(String period, String dateField) {
 		return dateField + "";
+	}
+	
+	protected void setTimeSpan(int span) {
+		mTimeSpan = span;
 	}
 	
 	
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
 		boolean result = super.onCreateOptionsMenu(menu);
-		menu.add(0, MENU_EMAIL, 1, R.string.email).setIcon(android.R.drawable.ic_menu_send);
+		menu.add(0, MENU_MONTH, 1, R.string.monthly).setIcon(android.R.drawable.ic_menu_month);
+		menu.add(0, MENU_YEAR, 1, R.string.service_year).setIcon(android.R.drawable.ic_menu_my_calendar);
+		menu.add(0, MENU_EMAIL, 1, R.string.send).setIcon(android.R.drawable.ic_menu_send);
 		return result;
     }
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {		
 		switch(item.getItemId()) {
+		
+		case MENU_MONTH:
+			setTimeSpan(MENU_MONTH);
+			break;
+		case MENU_YEAR:
+			setTimeSpan(MENU_YEAR);
+			break;
 		
 		case MENU_EMAIL:
 			sendEmail();
@@ -153,7 +176,7 @@ public class StatisticsActivity extends Activity {
 		i.putExtra(Intent.EXTRA_EMAIL, new String[] {});  
 		i.putExtra(Intent.EXTRA_SUBJECT, "Service Time for " + mCurrentTimePeriod);  
 		i.putExtra(Intent.EXTRA_TEXT, "body goes here");
-		startActivity(Intent.createChooser(i, "Send e-mail..."));
+		startActivity(Intent.createChooser(i, "Send by..."));
 	}
 	
 }

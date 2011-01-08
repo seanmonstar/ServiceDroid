d33d78fb6ce9e311028840a00a41b1a198669498
Sean <sean.monstar@gmail.com>
2011-01-08 00:26:26 -0800

    some serious backing up action

--- src/com/monstarlab/servicedroid/model/ServiceDroidBackupAgent.java
+++ src/com/monstarlab/servicedroid/model/ServiceDroidBackupAgent.java
@@ -1,5 +1,9 @@
 package com.monstarlab.servicedroid.model;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import android.app.backup.BackupAgent;
@@ -9,6 +13,8 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
 public class ServiceDroidBackupAgent extends BackupAgent {
+
+	private static final String APP_DATA_KEY = "alldata";
 
 	@Override
 	public void onBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState) throws IOException {
@@ -24,7 +30,20 @@ public class ServiceDroidBackupAgent extends BackupAgent {
 		if (doBackup) {
 			BackupWorker bw = new BackupWorker();
 			String serializedData = bw.backup(getContentResolver());
-			
+
+			ByteArrayOutputStream bufStream = new ByteArrayOutputStream();
+
+            // We use a DataOutputStream to write structured data into
+            // the buffering stream
+            DataOutputStream outWriter = new DataOutputStream(bufStream);
+            outWriter.writeChars(serializedData);
+
+            // Okay, we've flattened the data for transmission.  Pull it
+            // out of the buffering stream object and send it off.
+            byte[] buffer = bufStream.toByteArray();
+            int len = buffer.length;
+            data.writeEntityHeader(APP_DATA_KEY, len);
+            data.writeEntityData(buffer, len);
 		}
 		
 		//record a newState
@@ -32,8 +51,26 @@ public class ServiceDroidBackupAgent extends BackupAgent {
 
 	@Override
 	public void onRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState) throws IOException {
-		// TODO Auto-generated method stub
-
+		while (data.readNextHeader()) {
+			String key = data.getKey();
+			int dataSize = data.getDataSize();
+			
+			if (APP_DATA_KEY.equals(key)) {
+				 byte[] dataBuf = new byte[dataSize];
+                data.readEntityData(dataBuf, 0, dataSize);
+                ByteArrayInputStream baStream = new ByteArrayInputStream(dataBuf);
+                DataInputStream in = new DataInputStream(baStream);
+                
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = in.readLine()) != null) {
+                	sb.append(line);
+                }
+                String serializedData = sb.toString();
+                
+                //import data into the DB via BackupWorker
+			}
+		}
 	}
 
 }

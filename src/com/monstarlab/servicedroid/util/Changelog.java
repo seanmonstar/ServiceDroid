45c6d35d320c64f9fb01ce0db22b8a73646ba60f
Sean McArthur <sean.monstar@gmail.com>
2011-12-06 16:20:29 -0800

    v1.4.5

--- src/com/monstarlab/servicedroid/util/Changelog.java
+++ src/com/monstarlab/servicedroid/util/Changelog.java
@@ -28,6 +28,7 @@ public class Changelog {
 	//private static final int V_1_4_2 = 14; //just bug fixes, no changelog needed
 	//private static final int V_1_4_3 = 15; //just bug fixes, no changelog needed
 	//private static final int V_1_4_4 = 16; //just bug fixes, no changelog needed
+	//private static final int V_1_4_5 = 15; //just bug fixes, no changelog needed
 	
 	private static final int CURRENT_VERSION = V_1_4;
 

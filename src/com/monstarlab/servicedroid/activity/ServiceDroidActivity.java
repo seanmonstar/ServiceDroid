7842b307c4af529fb0c624d08b4908dccac13a34
Sean McArthur <sean.monstar@gmail.com>
2012-10-05 16:18:25 -0700

    change TabActivity to show buttons to the main areas

--- src/com/monstarlab/servicedroid/activity/ServiceDroidActivity.java
+++ src/com/monstarlab/servicedroid/activity/ServiceDroidActivity.java
@@ -5,64 +5,52 @@ import com.monstarlab.servicedroid.service.ReminderService;
 import com.monstarlab.servicedroid.util.Changelog;
 
 import android.app.Activity;
-import android.app.TabActivity;
+import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TabHost;
 
-public class ServiceDroidActivity extends TabActivity implements TabHost.OnTabChangeListener {
-    
-	private TabHost mTabHost;
+public class ServiceDroidActivity extends Activity implements View.OnClickListener {
 	
-	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        mTabHost = getTabHost();
-        mTabHost.setOnTabChangedListener(this);
+        setContentView(R.layout.main);
         
-        setupTimeActivity();
-        setupReturnVisitsActivity();
-        setupStatisticsActivity();
+        findViewById(R.id.time).setOnClickListener(this);
+        findViewById(R.id.calls).setOnClickListener(this);
+        findViewById(R.id.stats).setOnClickListener(this);
         
         showWhatsNew();
         
         //setup reminders
         setupReminderService();
     }
-
-	public void setupTimeActivity() {
-		Intent intent = new Intent(this, TimeActivity.class);
-        mTabHost.addTab(mTabHost.newTabSpec("time")
-                .setIndicator(getString(R.string.time), getResources().getDrawable(R.drawable.clock))
-                .setContent(intent));
-	}
 	
-	public void setupReturnVisitsActivity() {
-		Intent intent = new Intent(this, CallsActivity.class);
-        mTabHost.addTab(mTabHost.newTabSpec("rvs")
-                .setIndicator(getString(R.string.callbook), getResources().getDrawable(R.drawable.home))
-                .setContent(intent));
-	}
-	
-	public void setupStatisticsActivity() {
-		Intent intent = new Intent(this, StatisticsActivity.class);
-        mTabHost.addTab(mTabHost.newTabSpec("stats")
-                .setIndicator(getString(R.string.stats), getResources().getDrawable(R.drawable.calendar))
-                .setContent(intent));
-	}
-	
-	public void launchTimeView() {
-		Intent i = new Intent(this, TimeActivity.class);
-		this.startActivity(i);
-	}
-
-	public void onTabChanged(String tabId) {
-		Activity activity = getLocalActivityManager().getActivity(tabId);
-        if (activity != null) {
-            activity.onWindowFocusChanged(true);
-        }
+	public void onClick(View view) {
+		Intent i = null;
+		
+		switch (view.getId()) {
+		
+		case R.id.time:
+			i = new Intent(this, TimeActivity.class);
+			break;
+		
+		case R.id.calls:
+			i = new Intent(this, CallsActivity.class);
+			break;
+			
+		case R.id.stats:
+			i = new Intent(this, StatisticsActivity.class);
+			break;
+			
+		default:
+			// tapped something else? whatevs...
+		}
+		
+		if (i != null) {
+			startActivity(i);
+		}
 	}
 	
 	private void setupReminderService() {
@@ -73,4 +61,6 @@ public class ServiceDroidActivity extends TabActivity implements TabHost.OnTabCh
 	private void showWhatsNew() {
 		Changelog.showFirstTime(this);
 	}
+
+	
 }
